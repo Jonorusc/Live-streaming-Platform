90fa447b3dcf9f412379234b744b685d68e81e4a
João Lucas <joaolucascp16@gmail.com>
2023-10-31 10:13:43 -0300

    raw: storybook: using new addons, themes and decorators

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,17 +1,23 @@
 import type { StorybookConfig } from '@storybook/nextjs'
 
+import path from 'path'
+
+const toPath = (_path) => path.join(process.cwd(), _path)
+
 const config: StorybookConfig = {
   stories: [
     // '../stories/**/*.mdx',
     // '../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)',
-    '../components/**/stories.tsx',
-    '../components/**/*.stories.tsx'
+    '../components/**/stories.tsx'
+    // '../components/**/*.stories.tsx'
   ],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
     '@storybook/addon-onboarding',
-    '@storybook/addon-interactions'
+    '@storybook/addon-interactions',
+    '@storybook/addon-styling',
+    '@storybook/addon-themes'
   ],
   framework: {
     name: '@storybook/nextjs',
@@ -19,6 +25,18 @@ const config: StorybookConfig = {
   },
   docs: {
     autodocs: 'tag'
+  },
+  webpackFinal: async (storybookWebpackConfig, { configType }) => {
+    const { resolve = {} } = storybookWebpackConfig
+    const { alias = {} } = resolve
+    // Add the '@' alias to ./
+    alias['@'] = toPath('.')
+
+    // Update the config
+    storybookWebpackConfig.resolve = { ...resolve, alias }
+
+    // Return the updated config
+    return storybookWebpackConfig
   }
 }
 export default config
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,9 @@
 import type { Preview } from '@storybook/react'
+import { withThemeFromJSXProvider } from '@storybook/addon-styling'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from '../styles/themes/default-theme'
+import { darkTheme } from './../styles/themes/dark-theme'
+import GlobalStyles from '../styles/themes/global-styles'
 
 const preview: Preview = {
   parameters: {
@@ -11,5 +16,13 @@ const preview: Preview = {
     }
   }
 }
+
+export const decorators = [
+  withThemeFromJSXProvider({
+    themes: { light: defaultTheme, dark: darkTheme },
+    Provider: ThemeProvider,
+    GlobalStyles: GlobalStyles
+  })
+]
 
 export default preview
