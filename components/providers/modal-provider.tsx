3a2531ea3e87046e54eafba12dadb41fac3be676
João Lucas <joaolucascp16@gmail.com>
2023-10-31 18:53:31 -0300

    Update modal-provider.tsx

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -1,9 +1,15 @@
 'use client'
 
 import { useEffect, useState } from 'react'
+import { AnimatePresence } from 'framer-motion'
+
+import SignUpModal from '@/components/modals/sign-up'
+
+import { useModal } from '@/hooks/use-modal'
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false)
+  const { isOpen, type  } = useModal()
 
   useEffect(() => {
     setIsMounted(true)
@@ -13,5 +19,9 @@ export const ModalProvider = () => {
     return null
   }
 
-  return null
+  return (
+    <AnimatePresence>
+      {isOpen && type === 'signup' && <SignUpModal />}
+    </AnimatePresence>
+  )
 }
