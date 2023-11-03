658908a2b8897d0053befaec63f6d8241c9fce65
João Lucas <joaolucascp16@gmail.com>
2023-11-03 20:23:54 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,34 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: 'export', // for next export static site
+  output: 'export', // for next export static site
   reactStrictMode: true,
   swcMinify: true,
   compiler: {
     styledComponents: true
+    //   styledComponents: boolean | {
+    //     // Enabled by default in development, disabled in production to reduce file size,
+    //     // setting this will override the default for all environments.
+    //     displayName?: boolean,
+    //     // Enabled by default.
+    //     ssr?: boolean,
+    //     // Enabled by default.
+    //     fileName?: boolean,
+    //     // Empty by default.
+    //     topLevelImportPaths?: string[],
+    //     // Defaults to ["index"].
+    //     meaninglessFileNames?: string[],
+    //     // Enabled by default.
+    //     cssProp?: boolean,
+    //     // Empty by default.
+    //     namespace?: string,
+    //     // Not supported yet.
+    //     minify?: boolean,
+    //     // Not supported yet.
+    //     transpileTemplateLiterals?: boolean,
+    //     // Not supported yet.
+    //     pure?: boolean,
+    //   },
+    // },
   },
   transpilePackages: ['lucide-react']
   // experimental: {
