93279ccdbd675ccd02f72afdb4fb31e247a02f8d
João Lucas <joaolucascp16@gmail.com>
2023-11-03 20:31:07 -0300

    feat: usePosition hook
    it gives us positions based on the mouse cursor

---
+++ hooks/use-position.ts
@@ -0,0 +1,52 @@
+import { useState, MouseEvent, useCallback } from 'react'
+
+interface Position {
+  x: number
+  y: number
+}
+
+const useMenuPosition = (
+  callback?: () => void
+): [Position, (event: MouseEvent<HTMLElement>) => void] => {
+  const [position, setPosition] = useState<Position>({ x: 0, y: 0 })
+
+  const handleClick = useCallback(
+    (event: MouseEvent<HTMLElement>) => {
+      const target = event.target as HTMLElement
+      const clickX = event.clientX
+      const clickY = event.clientY
+      const screenW = window.innerWidth
+      const screenH = window.innerHeight
+      const menuW = target.getBoundingClientRect().width
+      const menuH = target.getBoundingClientRect().height
+      const parentRect = target.parentElement?.getBoundingClientRect()
+
+      let offsetX = clickX
+      let offsetY = clickY
+
+      if (parentRect) {
+        if (clickX + menuW > screenW) {
+          offsetX = parentRect.left - menuW
+        }
+
+        if (clickY + menuH > screenH) {
+          offsetY = parentRect.top - menuH
+        }
+      }
+
+      setPosition({
+        x: offsetX,
+        y: offsetY
+      })
+
+      if (callback) {
+        callback()
+      }
+    },
+    [callback]
+  )
+
+  return [position, handleClick]
+}
+
+export default useMenuPosition
