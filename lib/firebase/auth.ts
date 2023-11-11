bce294150f38c45b98812f243bfd7680fd57aca5
João Lucas <joaolucascp16@gmail.com>
2023-11-11 16:50:03 -0300

    feat: firebase auth (getCurrentUser)

--- lib/firebase/auth.ts
+++ lib/firebase/auth.ts
@@ -14,23 +14,24 @@ import { app_config } from '@/utils/firebase'
 const app = initializeApp(app_config)
 const auth = getAuth(app)
 
-type UserReturnProps = {
+export type UserReturnProps = {
   message: string
+  error?: {
+    [key: string]: string
+  }
   data: User | null
 }
 
 // fuction for checking if user is logged in (get the current user)
-export const getCurrentUser = () => {
-  onAuthStateChanged(auth, (user) => {
-    if (user) {
-      // User is signed in, see docs for a list of available properties
-      // https://firebase.google.com/docs/reference/js/auth.user
-      return user
-      // ...
-    } else {
-      // User is signed out
-      return null
-    }
+export const getCurrentUser = (): Promise<User | null> => {
+  return new Promise((resolve, reject) => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        resolve(user)
+      } else {
+        reject(null)
+      }
+    })
   })
 }
 
@@ -78,26 +79,65 @@ export const signin = (
           case AuthErrorCodes.INVALID_EMAIL:
             reject({
               message: 'The email address is incorrect.',
+              error: {
+                email: 'The email address is incorrect.'
+              },
               data: null
             })
             break
           case AuthErrorCodes.INVALID_PASSWORD:
             reject({
               message: 'The email password is incorrect.',
+              error: {
+                password: 'The password is incorrect.'
+              },
               data: null
             })
             break
           case AuthErrorCodes.USER_MISMATCH:
             reject({
               message: 'There is no user corresponding to the given email.',
+              error: {
+                user: 'There is no user corresponding to the given email.'
+              },
               data: null
             })
             break
-          default:
+          case AuthErrorCodes.CREDENTIAL_MISMATCH:
+            reject({
+              message: 'Invalid authentication credentials provided.',
+              error: {
+                auth: 'Invalid authentication credentials provided.'
+              },
+              data: null
+            })
+            break
+          case AuthErrorCodes.TOO_MANY_ATTEMPTS_TRY_LATER:
+            reject({
+              message:
+                'Access to this account has been temporarily disabled due to many failed login attempts. You can immediately restore it by resetting your password or you can try again later.',
+              error: {
+                auth: 'Access to this account has been temporarily disabled due to many failed login attempts. You can immediately restore it by resetting your password or you can try again later.'
+              },
+              data: null
+            })
+            break
+          default: {
+            if (error.message.includes('auth/invalid-login-credentials')) {
+              reject({
+                message: 'Invalid login credentials provided.',
+                error: {
+                  auth: 'Invalid login credentials provided.'
+                },
+                data: null
+              })
+              return
+            }
             reject({
               message: error.message,
               data: null
             })
+          }
         }
       })
   })
