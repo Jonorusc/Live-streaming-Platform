dc58d8a0cd364550ccea5be6fed52f49ff20e55d
João Lucas <joaolucascp16@gmail.com>
2023-11-11 16:49:26 -0300

    feat: using sounds on notifications

--- hooks/use-toast.ts
+++ hooks/use-toast.ts
@@ -1,15 +1,29 @@
 import { create } from 'zustand'
+import PlaySound from '@/utils/play-sound'
 
-type MESSAGE = TemplateStringsArray | Record<string, any> | string
-type POSITION = 'top-right' | 'top-left' | 'bottom-right' | 'bottom-left'
+export type POSITION = 'top-right' | 'top-left' | 'bottom-right' | 'bottom-left'
+export type TYPE = 'success' | 'error' | 'warning' | 'info' | 'islive'
+export interface ToastData {
+  profile?: {
+    picture: string
+    name: string
+  }
+  redirect?: string
+  title?: string
+  message: string
+}
 
-export interface Toast {
+export type Toast = {
   id: number
-  message: MESSAGE
-  type: 'success' | 'error' | 'warning' | 'info'
+  type: TYPE
   position: POSITION
   timeout: number
-  data?: any
+  remainingTime?: number
+  startTime?: number
+  data: ToastData
+  sound?: boolean
+  hovered?: boolean
+  timerId?: NodeJS.Timeout | number
 }
 
 export type ToastProps = {
@@ -17,22 +31,73 @@ export type ToastProps = {
   show: boolean
   addToast: (toast: Toast) => void
   removeToast: (id: number) => void
+  pauseToast: (id: number) => void
+  resumeToast: (id: number) => void
 }
 
 export const useToast = create<ToastProps>((set, get) => ({
   toasts: [],
   show: false,
   addToast: (toast) => {
+    if (toast.sound) {
+      if (toast.type === 'islive') {
+        PlaySound('/sounds/live_notification.mp3')
+      } else {
+        PlaySound('/sounds/new_notification.mp3')
+      }
+    }
+
+    toast.hovered = false
+    toast.remainingTime = toast.timeout
+    toast.startTime = Date.now()
     set((state) => ({ toasts: [...state.toasts, toast], show: true }))
 
-    setTimeout(() => {
-      get().removeToast(toast.id)
+    toast.timerId = setTimeout(() => {
+      if (!toast.hovered) {
+        get().removeToast(toast.id)
+      }
     }, toast.timeout)
   },
-  removeToast: (id) => {
+  removeToast: (id: number) => {
+    if (!get().toasts.length) return
+    set((state) => ({
+      toasts: state.toasts.filter((toast) => toast.id !== id),
+      show: state.toasts.length > 1
+    }))
+  },
+  pauseToast: (id: number) => {
+    if (!get().toasts.length) return
     set((state) => {
-      const newToasts = state.toasts.filter((t) => t.id !== id)
-      return { toasts: newToasts, show: newToasts.length > 0 }
+      const toast = state.toasts.find((toast) => toast.id === id)
+      if (toast) {
+        toast.hovered = true
+        if (toast.timerId) {
+          clearTimeout(toast.timerId)
+          if (
+            toast.remainingTime !== undefined &&
+            toast.startTime !== undefined
+          ) {
+            toast.remainingTime -= Date.now() - toast.startTime
+          }
+        }
+      }
+      return { toasts: [...state.toasts] }
+    })
+  },
+  resumeToast: (id: number) => {
+    if (!get().toasts.length) return
+    set((state) => {
+      const toast = state.toasts.find((toast) => toast.id === id)
+      if (toast) {
+        toast.hovered = false
+        toast.startTime = Date.now()
+        toast.timerId = setTimeout(() => {
+          if (!toast.hovered) {
+            get().removeToast(toast.id)
+          }
+        }, toast.remainingTime)
+      }
+      return { toasts: [...state.toasts] }
     })
   }
 }))
