0f53fba719203424c756ef24bef802b69d712988
João Lucas <joaolucascp16@gmail.com>
2023-11-12 19:06:56 -0300

    raw: changes the user api route return when there is no token

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -6,7 +6,7 @@ export async function GET(req: Request) {
   const token = cookies().get('user_token_id')
 
   if (!token) {
-    return new NextResponse('Unauthorized', { status: 401 })
+    return NextResponse.json({ error: { message: 'Unauthorized' } })
   }
 
   const firebase_id = token.value
