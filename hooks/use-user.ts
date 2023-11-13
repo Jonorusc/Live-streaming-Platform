c48d55b07c359c900a2bcb4405019212d99f9843
João Lucas <joaolucascp16@gmail.com>
2023-11-13 17:33:00 -0300

    raw: adds type to useUser hook

--- hooks/use-user.ts
+++ hooks/use-user.ts
@@ -1,21 +1,39 @@
 import useSWR from 'swr'
 import { getFetcher } from '@/utils/axios-fetcher'
+import { Profile, User, Channel, Follower, Subscriber } from '@prisma/client'
 
 const options = {
   revalidateOnReconnect: true,
   shouldRetryOnError: false
 }
 
-export function useUser() {
-  const { data, error, mutate, isValidating } = useSWR(
+type UserProps = User & {
+  profile?: Profile
+  channel?: Channel
+  follows?: Follower[]
+  subscribers?: Subscriber[]
+}
+
+export function useUser(): {
+  user: UserProps | null
+  isValidating: boolean
+  isLoading: boolean
+  userError: any
+  userMutate: (
+    data?: any,
+    shouldRevalidate?: boolean
+  ) => Promise<any | undefined>
+} {
+  const { data, error, mutate, isValidating, isLoading } = useSWR(
     `/api/user`,
     getFetcher,
     options
   )
 
   return {
-    user: data,
-    userLoading: isValidating,
+    user: data?.error ? null : data,
+    isValidating, //true if revalidation is in progress
+    isLoading, //nothing to display
     userError: error,
     userMutate: mutate
   }
