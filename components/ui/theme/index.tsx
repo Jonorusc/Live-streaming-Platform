9af17a3d238ffdf10fe23b618db60824f1ed136f
João Lucas <joaolucascp16@gmail.com>
2023-11-23 00:10:19 -0300

    refactor: using Toggler modal to change theme

--- components/ui/theme/index.tsx
+++ components/ui/theme/index.tsx
@@ -1,76 +1,37 @@
 'use client'
-import { Wrapper, List, ListItem } from './styles'
+import { Wrapper } from './styles'
 
 import { defaultTheme } from '@/styles/themes/default-theme'
 import { darkTheme } from '@/styles/themes/dark-theme'
 import Flex from '@/components/ui/flex'
-
-import useClickOutside from '@/hooks/use-clickoutside'
-import useMenuPosition from '@/hooks/use-position'
-import { useLocalStorage } from 'usehooks-ts'
-import { AnimatePresence } from 'framer-motion'
-import { useState, useRef } from 'react'
-import { Moon, Sun, SunMoon, ChevronRight } from 'lucide-react'
 import NoSsr from '@/components/NoSsr'
 
+import { useLocalStorage, useReadLocalStorage } from 'usehooks-ts'
+import { Moon } from 'lucide-react'
+import Toggler from '@/components/ui/toggler'
+import { DefaultTheme } from 'styled-components'
+import { useEffect, useState } from 'react'
+
 const ThemeSwitcher = () => {
-  const [isOpen, setIsOpen] = useState(false)
-  const wrapperRef = useRef<HTMLDivElement>(null)
-
   const [, setCurrentTheme] = useLocalStorage('theme', defaultTheme)
-
-  const [position, handleClick] = useMenuPosition(() => setIsOpen(true))
-
-  useClickOutside(wrapperRef, () => {
-    setIsOpen(false)
-  })
-
+  const currentTheme: DefaultTheme | null = useReadLocalStorage('theme')
+  const [active] = useState<boolean>(currentTheme?.name === 'dark')
   return (
     <NoSsr>
-      <Wrapper onClick={handleClick} ref={wrapperRef}>
-        <Flex $align="center" $justify="space-between">
+      <Wrapper>
+        <Flex $align="center" $justify="space-between" $width="100%">
           <Flex $align="center" $gapY="0.5rem">
-            <SunMoon size={20} />
-            <span>Themes</span>
+            <Moon size={20} />
+            <span>Dark Theme</span>
           </Flex>
-          <ChevronRight size={20} />
+          <Toggler
+            id="theme-switcher"
+            active={active}
+            onSwitch={(model) => {
+              setCurrentTheme(model ? darkTheme : defaultTheme)
+            }}
+          />
         </Flex>
-        <AnimatePresence>
-          {isOpen && (
-            <List
-              $x={position.x}
-              $y={position.y}
-              onClick={(e) => {
-                e.stopPropagation()
-              }}
-            >
-              <ListItem
-                onClick={(e) => {
-                  e.stopPropagation()
-                  setCurrentTheme(defaultTheme)
-                  setIsOpen(false)
-                }}
-              >
-                <Flex $align="center" $gapY="1rem">
-                  <Sun size={18} />
-                  <span>Light</span>
-                </Flex>
-              </ListItem>
-              <ListItem
-                onClick={(e: React.MouseEvent<HTMLButtonElement>) => {
-                  e.stopPropagation()
-                  setCurrentTheme(darkTheme)
-                  setIsOpen(false)
-                }}
-              >
-                <Flex $align="center" $gapY="1rem">
-                  <Moon size={18} />
-                  <span>Dark</span>
-                </Flex>
-              </ListItem>
-            </List>
-          )}
-        </AnimatePresence>
       </Wrapper>
     </NoSsr>
   )
