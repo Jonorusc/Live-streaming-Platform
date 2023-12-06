94371b73473e75120b3b1caa3c54ef505f69039b
João Lucas <joaolucascp16@gmail.com>
2023-12-06 11:49:08 -0300

    feat: crooper component

---
+++ components/ui/crooper/index.tsx
@@ -0,0 +1,64 @@
+'use client'
+
+import NoSsr from '@/components/NoSsr'
+import * as S from './styles'
+
+import { useState, useCallback, SetStateAction, Dispatch } from 'react'
+import Cropper from 'react-easy-crop'
+import { Point, Area } from 'react-easy-crop/types'
+
+export type CrooperProps = {
+  imgSrc: string
+  $width?: string
+  $height?: string
+  onCrop: (croppedArea: Area, croppedAreaPixels: Area) => void
+  onCancel?: () => void
+  shape?: 'rect' | 'round'
+  aspectRatio?: number
+  grid?: boolean
+  zoom: number
+  setZoom: Dispatch<SetStateAction<number>>
+}
+
+const Crooper = ({
+  imgSrc,
+  aspectRatio = 1 / 1,
+  shape = 'round',
+  onCrop,
+  $width,
+  $height,
+  grid = false,
+  zoom,
+  setZoom
+}: CrooperProps) => {
+  const [crop, setCrop] = useState<Point>({ x: 0, y: 0 })
+
+  const onCropComplete = useCallback(
+    (croppedArea: Area, croppedAreaPixels: Area) => {
+      onCrop(croppedArea, croppedAreaPixels)
+    },
+    []
+  )
+
+  return (
+    <NoSsr>
+      <S.Wrapper $height={$height} $width={$width}>
+        <S.CrooperWrapper>
+          <Cropper
+            image={imgSrc}
+            crop={crop}
+            zoom={zoom}
+            aspect={aspectRatio}
+            onCropChange={setCrop}
+            cropShape={shape}
+            onCropComplete={onCropComplete}
+            onZoomChange={setZoom}
+            showGrid={grid}
+          />
+        </S.CrooperWrapper>
+      </S.Wrapper>
+    </NoSsr>
+  )
+}
+
+export default Crooper
---
+++ components/ui/crooper/styles.ts
@@ -0,0 +1,27 @@
+import { lighten } from 'polished'
+import styled, { css } from 'styled-components'
+
+export const Wrapper = styled.div<{ $width?: string; $height?: string }>`
+  ${({ $height, $width }) => css`
+    width: ${$width || '100%'};
+    height: ${$height || '100%'};
+    position: absolute;
+    inset: 0;
+  `}
+`
+
+export const CrooperWrapper = styled.div`
+  ${({ theme }) => css`
+    position: absolute;
+    inset: 0;
+    height: 100%;
+    width: 100%;
+    bottom: 0.8rem;
+    .reactEasyCrop_CropArea {
+      color: ${theme.name === 'dark'
+        ? 'rgba(24, 24, 27, 0.6)'
+        : 'rgba(255, 255, 255, 0.5)'};
+      border: none;
+    }
+  `}
+`
