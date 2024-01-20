0afaaff06908306e2cd4fbc20715db212df729a0
Lucas <joaolucascp16@gmail.com>
2024-01-20 17:29:47 -0300

    fix: user email verification

--- app/(setup)/(routes)/(main)/user/verify/verify-email.ts
+++ app/(setup)/(routes)/(main)/user/verify/verify-email.ts
@@ -3,28 +3,22 @@ import { handleverifyemail } from '@/lib/firebase/actions'
 import { updateUserEmailStatus } from '@/actions/user'
 
 export const verifyEmail = async (actionCode: string) => {
-  // try to verify email
-  return new Promise(async (resolve, reject) => {
+  try {
     const firebaseUser = await getCurrentUser()
-    if (!firebaseUser || !firebaseUser.email) {
-      reject('You need to be logged in to verify your email.')
-      return
-    }
-    try {
-      // inside of handleverifyemail (firebase), we will check if the user is already logged in or not and check the action code as well
-      await handleverifyemail(actionCode).then(async () => {
-        // I'm using two methods to update the user's email status because at some point if I decide to switch to another authentication platform I can easily change the code
-        if (firebaseUser.email) {
-          console.log(firebaseUser.email)
-          await updateUserEmailStatus(firebaseUser.email)
-        } else {
-          reject('Something went wrong. Please try again later.')
-        }
-      })
 
-      resolve('Your email has been verified.')
-    } catch (error) {
-      reject(error || 'Something went wrong. Please try again later.')
+    if (!firebaseUser || !firebaseUser.email) {
+      throw new Error('You need to be logged in to verify your email.')
     }
-  })
+
+    await handleverifyemail(actionCode)
+    if (firebaseUser.email) {
+      await updateUserEmailStatus(firebaseUser.email)
+    } else {
+      throw new Error('Something went wrong. Please try again later.')
+    }
+
+    return 'Your email has been verified.'
+  } catch (error: any) {
+    throw new Error(error || 'Something went wrong. Please try again later.')
+  }
 }
